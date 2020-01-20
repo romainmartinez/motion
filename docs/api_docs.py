12e519e6fe37127b893ecc88cc64cc750891d83e
Romain Martinez <martinez.staps@gmail.com>
2020-01-20 11:13:08 -0500

    windows fix

--- docs/api_docs.py
+++
@@ -1,9 +0,0 @@
-import pdoc
-
-modules = ["motion", "motion.analogs"]
-context = pdoc.Context()
-
-modules = [pdoc.Module(mod, context=context) for mod in modules]
-pdoc.link_inheritance(context)
-
-modules[1].text()
