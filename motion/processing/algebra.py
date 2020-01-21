b25c97bbc496f10b0b0ddfd7203c9704a2bae292
romain <martinez.staps@gmail.com>
2020-01-21 10:42:45 -0500

    fix norm test

--- motion/processing/algebra.py
+++ motion/processing/algebra.py
@@ -17,10 +17,11 @@ def square(array: xr.DataArray, *args, **kwargs) -> xr.DataArray:
 
 
 def norm(array: xr.DataArray, dim: Union[str, list], ord: int = None) -> xr.DataArray:
-    if isinstance(dim, str):
-        dim = [dim]
     return xr.apply_ufunc(
-        np.linalg.norm, array, input_core_dims=[dim], kwargs={"ord": ord, "axis": -1}
+        np.linalg.norm,
+        array.drop_sel(axis="ones") if hasattr(array, "axis") else array,
+        input_core_dims=[[dim]] if isinstance(dim, str) else dim,
+        kwargs={"ord": ord, "axis": -1},
     )
 
 
